54f4ad8078223e1b09c67d3e5e739845647baa9a
Rogelio Guzman <rhuerta@twitter.com>
2015-09-30 11:42:16 -0700

    Simple example working

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
 import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+class Img extends Component {
+  filterNames = ['grayscale', 'blur', 'sepia', 'opacity', 'drop-shadow'];
 
   render() {
+    const filters = this.filterNames.map((filter) => {
+      const value = this.props[filter];
+      if (value) {
+        return `${filter}(${value})`
+      }
+    }).join(' ');
+
+    const style = {
+      WebkitFilter: filters
+    }
+
+    console.log(this.props.grayscale);
+    return <img src={this.props.src} style={style} />
+  }
+}
+
+class ImgControl extends Component {
+  render() {
     return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
+      <div>
+        <span>{this.props.name}</span>
+        <input type="range" onChange={this.props.onChange} max={this.props.max}/>
+        </div>
+    )
   }
 }
 
 export class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.handleControlChange = this.handleControlChange.bind(this);
+  }
+
+  handleControlChange(filterName, unit) {
+    return (e) => {
+      const newState = {};
+      newState[filterName] = e.target.value + unit;
+      this.setState(Object.assign({}, this.state, newState));
+    }
+  }
+
   render() {
+    const { grayscale, blur, sepia}  = this.state;
+    console.log(grayscale);
     return (
       <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
-      </div>
+        <ImgControl onChange={this.handleControlChange('grayscale', '%')} name="grayscale" />
+        <ImgControl onChange={this.handleControlChange('sepia', '%')} name="sepia" />
+        <ImgControl onChange={this.handleControlChange('blur', 'px')} name="blur" max='25'/>
+        <Img src="http://lorempixel.com/600/600/" grayscale={grayscale} blur={blur} sepia={sepia}}/>
+        </div>
     );
   }
 }
